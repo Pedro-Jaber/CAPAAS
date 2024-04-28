37268a3069ab525973ea6eb02765d31d789f2d68
Pedro Jaber <pedrojaberc@gmail.com>
2024-04-28 20:44:16 -0300

    add `dogSchema`

---
+++ model/model_dog.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const TagSchema = new Schema({ tag: String }, { _id: false });
+const DogShema = new Schema(
+  {
+    // Image ID as a 24-character hexadecimal string
+    mimetype: String, // Mimetype: image/exemple
+    size: Number, // bytes
+    tags: [TagSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//TODO Get image mimetype before saving
+//TODO Get image size before saving
+
+module.exports = mongoose.model("Dog", DogShema);
