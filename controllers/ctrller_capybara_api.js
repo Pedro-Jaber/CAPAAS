3c79ec3114a92d42f6fc8cb790dd35a8a8844644
Pedro Jaber <pedrojaberc@gmail.com>
2024-04-29 22:24:59 -0300

    inserting a capybara in the database

--- controllers/ctrller_capybara_api.js
+++ controllers/ctrller_capybara_api.js
@@ -1,3 +1,5 @@
+const Capybara = require("../model/model_capybara");
+
 module.exports.getRandonCapybara = async (req, res) => {
   // /capybara
   res.status(200).send("RandomCapybara");
@@ -6,15 +8,56 @@ module.exports.getRandonCapybara = async (req, res) => {
   // /capybara?html=true
 };
 
-module.exports.postCapybara = async (req, res) => {
-  // Get image from request
-  // Get image type to store in database
-  // Get image size to store in database
-  // Get image tags to store in database
-  // Save in information on data database and get info _id
-  // Change image name to _i
-  // Save image on public/capybara_images/
+module.exports.getToPostCapybara = (req, res) => {
+  res.status(200).render("postcapybara");
+};
 
-  //* Status OK
-  res.status(200).redirect("/home");
+module.exports.postCapybara = async (req, res) => {
+  const tags_string = req.body.tags; // Get image tags to store in database
+  const image = req.file; // Get image from request
+
+  const mimetype = image.mimetype; // Get image mimetype to store in database
+  const size = image.size; // Get image size to store in database
+  const blob = image.buffer; // Get Blob to store in database
+  const tags = tags_string.trim().toLowerCase().split(" ");
+
+  console.log(image);
+
+  console.log(mimetype);
+  console.log(size);
+  console.log(blob);
+  console.log(tags);
+
+  // const buffer64 = buffer.toString("base64"); // 33.33% bigger than the buffer
+  // console.log(buffer.length);
+  // console.log(buffer64.length);
+
+  // The image in the HTML
+  // res.send(
+  //   ` <img src="data: ${mimetype};base64, ${buffer64}" style="max-width: 100vw; max-height: 100vh;"> `
+  // );
+
+  // Just the image
+  // res.type(mimetype).send(buffer);
+
+  // Save image and information on data database
+  try {
+    await Capybara.create({
+      mimetype,
+      size,
+      blob,
+      tags,
+    }).then(() => {
+      //* Status 201 Created
+      // res.status(201).redirect("/home");
+      res.status(201).redirect("/capybara/post-capybara");
+    });
+  } catch (error) {
+    console.log(error);
+    //* Status 500 Internal Server Error
+    res.status(500).send("500 Internal Server Error");
+  }
+
+  //* Status 400 Bad Request //error in information
+  //res.status(400).send("400 Bad Request");
 };
--- routers/routers_capybara_api.js
+++ routers/routers_capybara_api.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
+const multer = require("multer"); // Allow to handle files received from forms
 const ctrllerCapybara = require("../controllers/ctrller_capybara_api");
 
 const router = Router();
 
+const storage = multer.memoryStorage(); // Set to save image in RAM memory
+const limits = { fileSize: 1024 * 1024 * 1 }; // file size in bytes // 1MB
+const upload = multer({
+  dest: "./public/images/temp",
+  storage: storage,
+  limits: limits,
+});
+
 router.route("").get(ctrllerCapybara.getRandonCapybara);
+router
+  .route("/post-capybara")
+  .get(ctrllerCapybara.getToPostCapybara)
+  .post(upload.single("image"), ctrllerCapybara.postCapybara);
 
 module.exports = router;
